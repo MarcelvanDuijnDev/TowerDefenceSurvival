1984b2e5e54cedec32832dabe04406b5053a6f17
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2018-09-13 23:51:08 +0200

    Commit

--- SurvivalGame/Assets/Game/Scripts/Editor/Tool_Scripts.cs
+++ SurvivalGame/Assets/Game/Scripts/Editor/Tool_Scripts.cs
@@ -12,13 +12,13 @@ public class Tool_Scripts : EditorWindow
     #region Scripts
     public string[] scriptName = new string[]
     {
-        "TestScript",
+        "Tool_Movement",
         "Test2"
     };
     public string[] scriptCode = new string[]
     {
         "Testing",
-        "Test2"
+        ""
     };
     #endregion
 
@@ -33,30 +33,39 @@ public class Tool_Scripts : EditorWindow
     {
         GUILayout.Label("Scripts", EditorStyles.boldLabel);
         GUILayout.BeginVertical("Box");
-        GUILayout.BeginVertical("Box");
-
-        GUILayout.EndVertical();
+        GUILayout.BeginHorizontal("Box");
+		searchScript = EditorGUILayout.TextField("Search: ", searchScript);
+        GUILayout.EndHorizontal();
 
 
         for (int i = 0; i < scriptName.Length; i++)
         {
-            GUILayout.BeginHorizontal("Box");
-            GUILayout.Label(scriptName[i], EditorStyles.boldLabel);
-            if (GUILayout.Button("Add"))
-            {
-                //Add script
-            }
-            if (GUILayout.Button("Copy"))
-            {
-                EditorGUIUtility.systemCopyBuffer = scriptName[i];
-            }
-            if (GUILayout.Button("Show"))
-            {
-                textField = scriptCode[i];
-            }
+			if (searchScript == "" || searchScript == null || scriptName[i].ToLower().Contains(searchScript.ToLower()))
+			{
+				GUILayout.BeginHorizontal("Box");
+				GUILayout.Label(scriptName[i], EditorStyles.boldLabel);
+				if (GUILayout.Button("Add"))
+				{
+					//Add script
+				}
+				if (GUILayout.Button("Copy"))
+				{
+					EditorGUIUtility.systemCopyBuffer = scriptCode[i];
+				}
+				if (GUILayout.Button("Show"))
+				{
+					textField = scriptCode[i];
+				}
+				GUILayout.EndHorizontal();
 
-
-            GUILayout.EndHorizontal();
+				/*
+				GUILayout.BeginScrollView(new Vector2(3,150), GUILayout.Width(position.width), GUILayout.Height(position.height - 100));
+				GUILayout.BeginHorizontal("Box");
+				//textField = EditorGUI.TextArea(new Vector2(0,0), textField);
+				GUILayout.EndHorizontal();
+				GUILayout.EndScrollView();
+				*/
+			}
         }
 
 
