196dc768f4f9850b91bef0017bf4936d8943161b
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2018-10-22 00:48:11 +0200

    Commit

---
+++ SurvivalGame/Assets/Game/Scripts/Editor/Tool_2DGridGame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+using System.IO;
+
+public class Tool_2DGridGame : EditorWindow
+{
+    private Vector2 m_Size;
+    private GameObject m_ParentObj;
+    private Vector4[] m_GridObjects;
+
+
+    [MenuItem("Tools/2DGridGame")]
+    static void Init()
+    {
+        Tool_2DGridGame window = (Tool_2DGridGame)EditorWindow.GetWindow(typeof(Tool_2DGridGame));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        
+    }
+}
+
